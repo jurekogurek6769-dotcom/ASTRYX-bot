f11a8f2ab0901a37d309318ef7c8be8efc82f088
jurekogurek6769-dotcom <jurekogurek6769@gmail.com>
2026-06-29 20:09:17 +0100

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,8 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "⚡ Stworzony dla społeczności ASTRYX.
+",
         // Activity type number (0 = Playing).
         type: 0,
       },
